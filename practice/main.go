c5af77683ad01f18739fc47421c5101e488a6df2
Abdul Waheed <ab.waheed1722@gmail.com>
2025-09-02 00:12:39 +0300

    input type conversion conversion

--- practice/main.go
+++ practice/main.go
@@ -4,13 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
-
-var pageSize = 25
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter Text: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Print("You Entered: ", input)
+
+	fmt.Print("What is your age:")
+
+	ageInput, _ := reader.ReadString('\n')
+
+	age, error := strconv.ParseFloat(strings.TrimSpace(ageInput), 64)
+
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println("Your age is: ", age)
+	}
+
 }
