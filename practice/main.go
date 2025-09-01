e4eb24e2b98445a2f03c26c45138f588cda508df
Abdul Waheed <ab.waheed1722@gmail.com>
2025-09-01 23:47:36 +0300

    variables

---
+++ practice/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var pageSize = 25
+
+func main() {
+	var age = 26.5
+	fmt.Println("Hello", pageSize)
+	fmt.Printf("Type is %T\n", pageSize)
+
+	fmt.Println("Age is ", age)
+	fmt.Printf("Age type %T\n", age)
+
+	name := "Abdul Waheed"
+
+	fmt.Println("My name is ", name)
+
+}
